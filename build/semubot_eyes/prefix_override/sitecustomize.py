0d8d4527f0e23ab6898369ef2ef969735aa95d25
Mykyta Nesterenko <nikitanes1006@gmail.com>
2026-04-23 20:41:11 +0300

    adding

---
+++ build/semubot_eyes/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/mykyta-nesterenko/Nesterenko-thesis-2026-ROS-stack/install/semubot_eyes'
